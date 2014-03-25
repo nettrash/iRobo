ad16a3518365834f8e5eb0ba5d05402b9f5e2c00
Ivan Alekseev <nettrash@NETTRASH-PRO.local>
2014-03-25 18:51:05 +0400

    Initial Commit

---
+++ iRoboTests/iRoboTests.m
@@ -0,0 +1,34 @@
+//
+//  iRoboTests.m
+//  iRoboTests
+//
+//  Created by Ivan Alekseev on 25.03.14.
+//  Copyright (c) 2014 ROBOKASSA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iRoboTests : XCTestCase
+
+@end
+
+@implementation iRoboTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
